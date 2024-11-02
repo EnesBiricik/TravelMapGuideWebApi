b1908f59155414343e398a7fb2959f0b8fad523a
ekremgunes <github1357@gmail.com>
2024-11-02 16:49:39 +0300

    endpoint changes test

--- TravelMapGuide.Web.Client/Front/src/constants/Endpoints.js
+++ TravelMapGuide.Web.Client/Front/src/constants/Endpoints.js
@@ -1,8 +1,8 @@
 
 export const API_ENDPOINTS = {
-    PRODUCTION_URL: "<production_ip>",
+    PRODUCTION_URL: "http://16.16.211.168:7018/",
     LOCAL_URL: "https://localhost:7018/",
     DOCKER_URL: "http://localhost:7018/",
 
-    DEFAULT_URL: "http://localhost:7018/",
+    DEFAULT_URL: "http://16.16.211.168:7018/",
 };
--- TravelMapGuide.Web.Client/Front/src/constants/Keys.js
+++ TravelMapGuide.Web.Client/Front/src/constants/Keys.js
@@ -1,3 +1,3 @@
 export const KEYS = {
-    MAP_API_KEY : "AIzaSyD10h4jKRrJJ-030g8TqyMO-7UY_JoTv9s",
+    MAP_API_KEY : "AIzaSyCoFonx58G8ynRX99HsD81Zh7Y8G65K6Kw",
 }
